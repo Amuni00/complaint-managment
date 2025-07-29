7bf9e909897cd71c6a9ff4de8bda5e0c7f3895b1
Amuni00 <asfawamanuel89@gmail.com>
2025-07-29 23:32:58 +0300

    structure creation

---
+++ src/main/java/com/bank/complaint_managment/ComplaintManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.bank.complaint_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/complaint_managment/ComplaintManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.complaint_managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
